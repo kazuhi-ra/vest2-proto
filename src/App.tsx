958df27226b03c84713549139b9624947c86e838
kazuhi-ra <kazuhira4126@gmail.com>
2020-10-25 01:54:29 +0900

    useState版の骨組みできた

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from 'react'
 
-function App() {
+import Image from 'Image'
+
+const heads = ['頭1', '頭2', '頭3', '頭4']
+const vests = ['胴1', '胴2', '胴3', '胴4']
+const foots = ['脚1', '脚2', '脚3', '脚4']
+
+type intervalID = null | NodeJS.Timeout
+
+const App = () => {
+  const [head, setHead] = useState(0)
+  const [vest, setVest] = useState(0)
+  const [foot, setFoot] = useState(0)
+  const [headId, setHeadId] = useState<intervalID>(null)
+  const [vestId, setVestId] = useState<intervalID>(null)
+  const [footId, setFootId] = useState<intervalID>(null)
+  const [isFirstTime, setFirstTime] = useState(true)
+
+  const [panelsLeft, setPanelsLeft] = useState(3)
+
+  const isPlaying = useMemo(() => !!(headId || vestId || footId), [
+    headId,
+    vestId,
+    footId,
+  ])
+
+  const start = () => {
+    if (isPlaying) return
+
+    setPanelsLeft(3)
+
+    setHeadId(
+      setInterval(() => {
+        const headsLength = Math.floor(Math.random() * heads.length)
+        setHead(headsLength)
+      }, 100)
+    )
+
+    setVestId(
+      setInterval(() => {
+        const vestsLength = Math.floor(Math.random() * vests.length)
+        setVest(vestsLength)
+      }, 100)
+    )
+
+    setFootId(
+      setInterval(() => {
+        const footsLength = Math.floor(Math.random() * foots.length)
+        setFoot(footsLength)
+      }, 100)
+    )
+  }
+
+  const stop = (
+    intervalID: intervalID,
+    setId: React.Dispatch<React.SetStateAction<intervalID>>
+  ) => {
+    if (!isPlaying) return
+    if (intervalID === null) return
+
+    clearInterval(intervalID)
+    setId(null)
+
+    setPanelsLeft(prev => prev - 1)
+
+    if (panelsLeft === 1 && isFirstTime === true) {
+      setFirstTime(false)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div>
+        <Image src={heads[head]} />
+        <Image src={vests[vest]} />
+        <Image src={foots[foot]} />
+      </div>
+      <div className='buttonWrapper'>
+        <div onClick={() => stop(headId, setHeadId)}>STOP</div>
+        <div onClick={() => stop(vestId, setVestId)}>STOP</div>
+        <div onClick={() => stop(footId, setFootId)}>STOP</div>
+      </div>
+      {isFirstTime ? (<div onClick={() => start()}>START</div>) : (
+      <div className='shareWrapper'>
+        <div onClick={() => start()}>もう一度</div>
+        <div>これにする</div>
+      </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Image.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+
+type Props = {
+  src: string
+}
+
+const Image: FC<Props> = ({ src }) => {
+  return <div>{src}</div>
+}
+
+// export default React.memo(Image)
+export default Image
